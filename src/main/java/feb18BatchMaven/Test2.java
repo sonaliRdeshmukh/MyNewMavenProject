ee1a6f3b6d8a98aa9a1bdbcee2113b36ef16b2c1
SonaliDeshmukh <deshmukhsonali2602@gmail.com>
2023-07-06 15:57:23 +0530

    [Sonali]  i just create test2 class

---
+++ src/main/java/feb18BatchMaven/Test2.java
@@ -0,0 +1,10 @@
+package feb18BatchMaven;
+
+public class Test2 {
+
+	public static void main(String[] args) 
+	{
+      System.out.println("Hii pallavi & Ruksar Thids is my Second test class plese accept");
+	}
+
+}
