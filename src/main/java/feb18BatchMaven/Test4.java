86e8ba6f7a1919b99bc55056a72e7830f4d60f02
PallaviNPustake <pallavinpustake@gmail.com>
2023-07-06 16:24:24 +0530

    [PallaviP] i have added test4 class.

---
+++ src/main/java/feb18BatchMaven/Test4.java
@@ -0,0 +1,13 @@
+package feb18BatchMaven;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a= 50;
+		System.out.println(a);
+		
+	}
+
+}
